8de5464f338be946313379eae8a7753fd29c9147
Siddhi Vinayak <siddhiv171@gmail.com>
2026-06-19 11:17:47 +0530

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -9,4 +9,17 @@ export const projects = [
     live: "https://event-planner-genk.vercel.app",
     status: "Completed"
   }
+
+
+ {
+    id: "02",
+    name: "ALUM SPHERE",
+    shortDesc: "Event design + interactive game",
+    fullDesc: "Designed a similar app to Linkden where student connet to thier college alumni and build and algorithm for that the people get automatically get connected with alumni and The platform has features like live chats and question and post ",
+    tech: ["Event Planning", "Event Management"],
+    github: "https://github.com/vinayaksiddhi/event_planner",
+    live: "https://event-planner-genk.vercel.app",
+    status: "Completed"
+  }
+ 
 ]
